11849b8831770fa9967812fa6d758a436597c7e3
DanaKim31 <danakim731@gmail.com>
2024-04-04 14:40:05 +0900

    Initial commit

---
+++ src/main/java/com/project/compagnoserver/CompagnoServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.compagnoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompagnoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompagnoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/compagnoserver/CompagnoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.compagnoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompagnoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
