445001791421f57f4134d2ced3effc9cccd4f89e
DanaKim31 <152463101+DanaKim31@users.noreply.github.com>
2024-04-05 17:12:27 +0900

    <feat> 동물등록_대행업체 데이터 파싱
    * build.gradle
      - poi dependency 추가
    * xls file import
    * data parsing

--- src/main/java/com/project/compagno/CompagnoServerApplication.java
+++ src/main/java/com/project/compagno/CompagnoServerApplication.java
@@ -1,4 +1,4 @@
-package com.project.compagnoserver;
+package com.project.compagno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
