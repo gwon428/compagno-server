481ed19cbd0d3fe4910f2094ca73bdf9aec64dcc
jungdj221 <152463270+jungdj221@users.noreply.github.com>
2024-04-09 18:27:46 +0900

    <feat> animal_board 파일업로드 진행중

---
+++ src/main/java/com/project/compagnoserver/domain/AnimalBoardImage.java
@@ -0,0 +1,36 @@
+package com.project.compagnoserver.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@Table(name = "animal_board_image")
+public class AnimalBoardImage {
+
+    @Id
+    @Column(name = "animal_image_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int animalImageCode;
+
+    @ManyToOne
+    @JoinColumn(name = "animal_board_code")
+    @JsonIgnore
+    private AnimalBoard animalBoard;
+
+    @Column(name = "animal_board_image")
+    private String animalBoardImage;
+}
+
+/*
+* animal_image_code INT PRIMARY KEY AUTO_INCREMENT,
+    animal_board_code int,
+    animal_board_image varchar(300)
+* */
