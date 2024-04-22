3205c28fc5034048a61857e5cc033cd6739f0689
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-04-22 18:21:08 +0900

    <fix>user securityConfig

--- src/main/java/com/project/compagnoserver/config/SecurityConfig.java
+++ src/main/java/com/project/compagnoserver/config/SecurityConfig.java
@@ -1,27 +1,32 @@
 package com.project.compagnoserver.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .httpBasic(basic -> basic.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/compagno/login").permitAll()
+                        .requestMatchers("/compagno/signUp").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterAfter(jwtAuthenticationFilter, CorsFilter.class)
                 .build();
     }
 }
