63af4d5aaa0ff506f4b830da78ba2b9b7d559c2b
jungdj221 <152463270+jungdj221@users.noreply.github.com>
2024-05-09 19:27:55 +0900

    <feat> 광고추천로직 시작 / 회원가입시 자동으로 로직 등록

--- src/main/java/com/project/compagnoserver/service/UserService.java
+++ src/main/java/com/project/compagnoserver/service/UserService.java
@@ -1,11 +1,20 @@
 package com.project.compagnoserver.service;
 
+import com.project.compagnoserver.domain.Animal.AnimalCategory;
+import com.project.compagnoserver.domain.Animal.QAdRecommendLogic;
+import com.project.compagnoserver.domain.Animal.QAnimalCategory;
 import com.project.compagnoserver.domain.user.User;
+import com.project.compagnoserver.repo.Animal.AdRecommendLogicDAO;
 import com.project.compagnoserver.repo.user.UserDAO;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,11 +22,36 @@ public class UserService {
 
     @Autowired
     private UserDAO userDao;
+    @Autowired
+    private AdRecommendLogicDAO logicDAO;
 
+    @Autowired
+    private JPAQueryFactory queryFactory;
+    @Autowired
+    private EntityManager entityManager;
+    private final QAnimalCategory qAnimalCategory = QAnimalCategory.animalCategory;
+
+
+    public List<AnimalCategory> categoryList(){
+        List<AnimalCategory> categoryList = queryFactory.selectFrom(qAnimalCategory).fetch();
+        return categoryList;
+    }
     // 회원가입
     public User create(User user) {
         return userDao.save(user);
     }
+    // 회원가입 후 추천로직 기본세팅
+    public void setLogic (User user){
+        List<AnimalCategory> category = categoryList();
+        for(AnimalCategory oneCategory : category){
+
+//            log.info("user : " + user.getUserId());
+//            log.info("category : " + oneCategory.getAnimalCategoryCode());
+//            log.info("=======================");
+            logicDAO.setLogicBase(user.getUserId(), oneCategory.getAnimalCategoryCode());
+        }
+    }
+
 
     public User login(String id, String password, PasswordEncoder encoder) {
         User user = userDao.findById(id).orElse(null);
